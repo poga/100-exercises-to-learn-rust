405a71e1ea8ddc5c0ee67aeb8779e1bd2c5b0250
Poga Po <hi@devpoga.org>
2024-05-18 19:12:54 +0800

    chapter 04

--- exercises/04_traits/09_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/09_assoc_vs_generic/src/lib.rs
@@ -12,6 +12,30 @@
 // interested in learning more about it.
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
+//
+//
+
+pub trait Power<Rhs> {
+    fn power(&self, rhs: Rhs) -> Self;
+}
+
+impl Power<u16> for u32 {
+    fn power(&self, rhs: u16) -> Self {
+        self.pow(rhs.into())
+    }
+}
+
+impl Power<u32> for u32 {
+    fn power(&self, rhs: u32) -> Self {
+        self.pow(rhs)
+    }
+}
+
+impl Power<&u32> for u32 {
+    fn power(&self, rhs: &u32) -> Self {
+        self.pow(*rhs)
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- exercises/04_traits/11_copy/src/lib.rs
+++ exercises/04_traits/11_copy/src/lib.rs
@@ -1,6 +1,7 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +9,28 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+use std::ops::{Add, AddAssign};
+
+impl Add for WrappingU32 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self::new(self.value.wrapping_add(rhs.value))
+    }
+}
+
+impl AddAssign for WrappingU32 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.value = self.value.wrapping_add(rhs.value);
+    }
+}
+
+impl From<u32> for WrappingU32 {
+    fn from(value: u32) -> Self {
+        Self::new(value)
     }
 }
 
